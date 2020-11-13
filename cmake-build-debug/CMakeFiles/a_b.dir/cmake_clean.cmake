1e47dac6881f55c6fd39adabef94a8efeccfc56c
MuLinjiu <1838287508@qq.com>
2020-11-13 16:36:28 +0800

    Submission

---
+++ cmake-build-debug/CMakeFiles/a_b.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a_b.dir/main.cpp.obj"
+  "a_b.exe"
+  "a_b.exe.manifest"
+  "a_b.pdb"
+  "liba_b.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a_b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
